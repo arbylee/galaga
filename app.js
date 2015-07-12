94a65187cf2cab00bc47e95278640da8050066eb
Richard Lee <rblee88@gmail.com>
2015-07-12 14:04:50 -0500

    Add arrow controls

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 var player;
+var cursors;
 
 function preload(){
   game.load.image('background', 'assets/background.png')
@@ -15,6 +16,21 @@ function create(){
 }
 
 function update(){
+  cursors = game.input.keyboard.createCursorKeys();
+  player.body.velocity.x = 0;
+  player.body.velocity.y = 0;
+  if(cursors.left.isDown){
+    player.body.velocity.x -= 150;
+  }
+  if(cursors.right.isDown){
+    player.body.velocity.x += 150;
+  }
+  if(cursors.up.isDown){
+    player.body.velocity.y -= 150;
+  }
+  if(cursors.down.isDown){
+    player.body.velocity.y += 150;
+  }
 }
 
 
