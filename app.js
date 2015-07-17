e26147d4dee421a76a27b66503facbfe69cefd06
Richard Lee <rblee88@gmail.com>
2015-07-17 16:13:09 -0500

    Add enemy that dies to bullets

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ PhaserGame.prototype = {
   preload: function(){
     this.game.load.image('background', 'assets/background.png')
     this.game.load.image('ship', 'assets/ship.png')
+    this.game.load.image('enemy', 'assets/enemy.png')
     for (var i = 1; i <= 1; i++)
     {
         this.load.image('bullet' + i, 'assets/bullet_' + i + '.png');
@@ -38,6 +39,14 @@ PhaserGame.prototype = {
     cursors = this.game.input.keyboard.createCursorKeys();
 
     this.weapons.push(new window['galaga'].Weapon.SingleBullet(this.game));
+
+    this.enemies = this.game.add.group();
+    this.enemies.createMultiple(20, 'enemy');
+    this.enemies.enableBody = true;
+    this.game.physics.arcade.enable(this.enemies);
+
+    var enemy = this.enemies.getFirstDead();
+    enemy.reset(400, 100);
   },
 
   update: function(){
@@ -58,6 +67,12 @@ PhaserGame.prototype = {
     if(this.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)){
       this.weapons[this.currentWeapon].fire(this.player);
     }
+
+    this.physics.arcade.overlap(this.weapons, this.enemies, this.weaponsHitEnemy, null, this);
+  },
+  weaponsHitEnemy: function(weapon, enemy){
+    weapon.kill();
+    enemy.kill();
   }
 }
 
